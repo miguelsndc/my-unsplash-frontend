a7abab4b9860a450b1c1524aaac0df8b7525d05b
miguelsndc <mglsndc@gmail.com>
2021-12-23 16:48:54 -0300

    feat: setup stitches

---
+++ stitches.config.ts
@@ -0,0 +1,48 @@
+import { createStitches } from '@stitches/react';
+
+export const { globalCss, styled, getCssText, theme } = createStitches({
+  theme: {
+    colors: {
+      black: '#000',
+      gray1: '#333',
+      gray2: '#4F4F4F',
+      gray3: '#BDBDBD',
+      green1: '#3DB46D',
+      red1: '#EB5757',
+    },
+    fontSizes: {
+      xs: '0.75rem',
+      sm: '0.875rem',
+      md: '1rem',
+      lg: '1.5rem',
+      xl: '1.75rem',
+    },
+    fonts: {
+      default: 'Inter, sans-serif',
+    },
+    fontWeights: {
+      regular: '400',
+      medium: '500',
+      semi: '600',
+      bold: '700',
+    },
+    letterSpacings: {
+      default: '-0.035em',
+    },
+    radii: {
+      default: 12,
+      rounded: 16,
+    },
+    shadows: {
+      danger: '0px 1px 6px rgba(235, 87, 87, 0.1)',
+      green: '0px 1px 6px rgba(61, 180, 109, 0.2)',
+    },
+    media: {
+      xs: '(max-width: 600px)',
+      sm: '(min-width: 600px)',
+      md: '(min-width: 768px)',
+      lg: '(min-width: 992px)',
+      xl: '(min-width: 1200px)',
+    },
+  },
+});
