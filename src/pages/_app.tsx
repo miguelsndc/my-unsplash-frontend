020f9ab7813449ce19a6babab5444d214a25bc87
miguelsndc <mglsndc@gmail.com>
2021-12-24 19:16:57 -0300

    feat: toaster; setup recoil

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,17 @@
 import type { AppProps } from 'next/app';
+import { Toaster } from 'react-hot-toast';
+import { RecoilRoot } from 'recoil';
 import { globalStyles } from 'src/styles/global';
 
 function MyApp({ Component, pageProps }: AppProps) {
   globalStyles();
 
-  return <Component {...pageProps} />;
+  return (
+    <RecoilRoot>
+      <Toaster />
+      <Component {...pageProps} />
+    </RecoilRoot>
+  );
 }
 
 export default MyApp;
