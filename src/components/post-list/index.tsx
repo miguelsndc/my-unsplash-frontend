8c00ee47d2a0a1d29b69008164c8cc323a34121e
miguelsndc <mglsndc@gmail.com>
2021-12-25 16:59:17 -0300

    feat(components/post-list): confirm post deletion; allow users to undo the deletion

--- src/components/post-list/index.tsx
+++ src/components/post-list/index.tsx
@@ -1,14 +1,174 @@
-import { useRecoilValue } from 'recoil';
+import { ChangeEvent, useState } from 'react';
+import toast, { CheckmarkIcon, ErrorIcon } from 'react-hot-toast';
+import { useRecoilState, useRecoilValue } from 'recoil';
+import { userState } from 'src/atoms/auth';
 import { postsState } from 'src/atoms/posts';
-import { Container, PostImage } from './styles';
+import { api } from 'src/services/api';
+import { Button } from '../button';
+import { Dialog } from '../dialog';
+import { Post } from '../header';
+import { Input } from '../input';
+import {
+  Container,
+  DeletePostButton,
+  Overlay,
+  PostContainer,
+  PostImage,
+  PostTitle,
+  ButtonWrapper,
+  CustomToast,
+} from './styles';
+
+type DeletePostResponse = {
+  message: string;
+  deletedPost: Post;
+};
 
 export const PostList = () => {
-  const posts = useRecoilValue(postsState);
+  const user = useRecoilValue(userState);
+  const [posts, setPosts] = useRecoilState(postsState);
+  const [isDialogOpened, setIsDialogOpened] = useState(false);
+  const [postToBeDeleted, setPostToBeDeleted] = useState<Post | null>(null);
+  const [canDelete, setCanDelete] = useState(false);
+  const [deleteConfirmationInput, setDeleteConfirmationInput] = useState('');
+
+  const comparisonTerm = `${user?.name}/${postToBeDeleted?.label}`;
+
+  const handleUndoDeletion = async (
+    postToBeRestored: Post,
+    postsBeforeDeletion: Post[]
+  ) => {
+    try {
+      await api.post<Post>(`/posts`, {
+        label: postToBeRestored.label,
+        url: postToBeRestored.url,
+      });
+
+      setPosts(postsBeforeDeletion);
+    } catch {
+      toast(
+        t => (
+          <CustomToast>
+            An error happened while recreating your post
+            <button
+              onClick={() => {
+                handleUndoDeletion(postToBeRestored, postsBeforeDeletion);
+                toast.dismiss(t.id);
+              }}
+            >
+              Try again
+            </button>
+          </CustomToast>
+        ),
+        {
+          icon: <ErrorIcon />,
+        }
+      );
+    }
+  };
+
+  const resetDeleteConfirmation = () => {
+    setPostToBeDeleted(null);
+    setCanDelete(false);
+    setDeleteConfirmationInput('');
+    setIsDialogOpened(false);
+  };
+
+  const handleDeletePost = async (post: Post | null) => {
+    if (!post) return;
+
+    resetDeleteConfirmation();
+
+    const {
+      data: { deletedPost },
+    } = await api.delete<DeletePostResponse>(`/posts/${post.id}`);
+
+    const postsBeforeDeletion = posts.slice();
+
+    const newPosts = posts.filter(p => p.id !== post.id);
+
+    setPosts(newPosts);
+
+    toast(
+      t => (
+        <CustomToast>
+          Post deleted successfully
+          <button
+            onClick={() => {
+              handleUndoDeletion(deletedPost, postsBeforeDeletion);
+              toast.dismiss(t.id);
+            }}
+          >
+            UNDO
+          </button>
+        </CustomToast>
+      ),
+      {
+        icon: <CheckmarkIcon />,
+      }
+    );
+  };
+
+  const handleShowDeleteConfirmationDialog = (post: Post) => {
+    setIsDialogOpened(true);
+    setPostToBeDeleted(post);
+  };
+
+  const handleConfirmationInputChange = (
+    e: ChangeEvent<HTMLInputElement>,
+    comparisonTerm: string
+  ) => {
+    const currentInput = e.target.value;
+    currentInput === comparisonTerm ? setCanDelete(true) : setCanDelete(false);
+    setDeleteConfirmationInput(currentInput);
+  };
 
   return (
     <Container>
+      <Dialog
+        title='Are you sure ?'
+        description={`Are you sure you want do delete "${postToBeDeleted?.label}" ?`}
+        opened={isDialogOpened}
+        onOpenedChange={setIsDialogOpened}
+      >
+        <span>
+          Type <strong>{comparisonTerm}</strong> to proceed.
+        </span>
+        <Input
+          type='text'
+          onChange={e => handleConfirmationInputChange(e, comparisonTerm)}
+          value={deleteConfirmationInput}
+          placeholder='Type the necessary term to continue'
+          css={{ margin: '1rem 0' }}
+        ></Input>
+        <ButtonWrapper>
+          <Button color='gray' onClick={resetDeleteConfirmation}>
+            Cancel
+          </Button>
+          <Button
+            color='red'
+            disabled={!canDelete}
+            onClick={() => handleDeletePost(postToBeDeleted)}
+          >
+            Delete
+          </Button>
+        </ButtonWrapper>
+      </Dialog>
       {posts.map(post => {
-        return <PostImage key={post.id} src={post.url} alt={post.label} />;
+        return (
+          <PostContainer key={post.id}>
+            {user?.id === post.userId && (
+              <DeletePostButton
+                onClick={() => handleShowDeleteConfirmationDialog(post)}
+              >
+                delete
+              </DeletePostButton>
+            )}
+            <PostTitle>{post.label}</PostTitle>
+            <Overlay></Overlay>
+            <PostImage src={post.url} alt={post.label} loading='lazy' />
+          </PostContainer>
+        );
       })}
     </Container>
   );
--- src/components/post-list/styles.ts
+++ src/components/post-list/styles.ts
@@ -2,7 +2,7 @@ import { styled } from 'stitches/config';
 
 export const Container = styled('div', {
   columnCount: 3,
-  columnGap: 'clamp(10px, 5vw, 1.25rem)',
+  columnGap: 30,
   maxWidth: 'calc(1368px + 1rem)',
   margin: '0 auto',
   marginTop: '2rem',
@@ -14,4 +14,77 @@ export const PostImage = styled('img', {
   display: 'block',
   borderRadius: '$default',
   width: '100%',
+});
+
+export const Overlay = styled('div', {
+  background: '$black',
+  inset: 0,
+  zIndex: 2,
+  borderRadius: '$default',
+  position: 'absolute',
+  opacity: 0.25,
+});
+
+export const DeletePostButton = styled('button', {
+  position: 'absolute',
+  top: 15,
+  right: 15,
+  borderRadius: 38,
+  padding: '0.5rem 1rem',
+  color: '#fff',
+  background: '$red1',
+  border: '2px solid $red1',
+  fontFamily: '$default',
+  cursor: 'pointer',
+  transition: 'filter .2s',
+  '&:hover': {
+    filter: 'brightness(0.9)',
+  },
+  zIndex: 3,
+});
+
+export const PostTitle = styled('h1', {
+  position: 'absolute',
+  bottom: 30,
+  left: 20,
+  color: '#fff',
+  fontSize: '$lg',
+  fontWeight: '$semi',
+  zIndex: 3,
+});
+
+export const PostContainer = styled('div', {
+  position: 'relative',
+  [`& ${Overlay}, ${DeletePostButton}, ${PostTitle}`]: {
+    display: 'none',
+  },
+  '&:hover': {
+    [`& ${Overlay}, ${DeletePostButton}, ${PostTitle}`]: {
+      display: 'block',
+    },
+  },
+});
+
+export const ButtonWrapper = styled('div', {
+  display: 'flex',
+  width: '100%',
+  justifyContent: 'flex-end',
+  alignItems: 'center',
+  gap: '1rem',
+});
+
+export const CustomToast = styled('span', {
+  button: {
+    textTransform: 'uppercase',
+    border: 'none',
+    padding: '0.5rem',
+    fontFamily: '$default',
+    borderRadius: '$default',
+    cursor: 'pointer',
+    marginLeft: '0.5rem',
+    transition: 'filter .2s',
+    '&:hover': {
+      filter: 'brightness(0.9)',
+    },
+  },
 });
